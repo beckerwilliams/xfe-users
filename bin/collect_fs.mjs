41ed4d018af063e6a13ba7e998f6a4269f4bdc43
Ron Williams <ron.williams@infosecglobal.com>
2021-08-06 16:24:55 -0500

    Final Week Checkpoint All Test Suites Complete Successfully * (Fixed suppression warning in collect_fs.mjs)
    ~r

--- bin/collect_fs.mjs
+++ bin/collect_fs.mjs
@@ -46,7 +46,7 @@ const test_collector = new Collector();
 
 // MAIN
 if (process.argv.length <= 2) {
-    console.log("Usage: ./test_ScanFs.mjs <!--suppress ALL --><Directory to Scan>");
+    console.log("Usage: ./test_ScanFs.mjs <Directory to Scan>");
     process.exit(-1);
 }
 let dir = process.argv[2];
